dea4f31889610d131b6812421402a3b29828201f
Natjira05 <s6406021621099@email.kmutnb.ac.th>
2023-07-18 17:16:58 +0700

    test1

---
+++ Lecture-03Array/ex01.js
@@ -0,0 +1,16 @@
+let furniture = ['Table', 'Chairs', 'Couch'] ;
+
+for ( let name of furniture) {
+  console.log(name);
+  // เป็นการเรียงลำดับของ array ที่เรากำหนดมา โดยเรียงจากบนลงล่าง
+  
+  for ( let char of name){
+    console.log(char);
+  // กำหนดค่า let มาใหม่ เพื่อแสดงค่าการเเยกคำของคำใน name ที่แสดงออกมาจาก ในลูป for แรก
+  // เพื่อแสดงตัวอักษรของคำใน name ออกมา
+  }
+
+
+}
+
+
